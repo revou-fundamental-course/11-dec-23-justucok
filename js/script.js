207ad42ec29b6d43a35a30c8375da9921b7b97a0
justucok <rahmanyw43@gmail.com>
2023-12-21 23:49:11 +0700

    final submit

--- js/script.js
+++ js/script.js
@@ -4,23 +4,53 @@ document.addEventListener("DOMContentLoaded", function() {
     userElement.textContent = username;
 });
 
-function getValue() {
-    var name = document.getElementById("nameInput").value;
-    var ttl = document.getElementById("ttlInput").value;
-    var pesan = document.getElementById("messageInput").value;
-    var cowo = document.getElementById("cowoRadio");
-    var cewe = document.getElementById("ceweRadio");
+let slideIndex = 0;
 
-    var Gender = "";
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
 
-    if (cowo.checked) {
-        Gender = cowo.value;
-    } else if (cewe.checked) {
-        Gender = cewe.value;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
 
-    document.getElementById("displayName").textContent = "Nama : " + name;
-    document.getElementById("displayTtl").textContent = "Tanggal Lahir : " + ttl;
-    document.getElementById("displayGender").textContent = "Jenis Kelamin : " + Gender;
-    document.getElementById("displayMessage").textContent = "Pesan : " + pesan;
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
+
+function getValue() {
+    var name = document.getElementById("nameInput").value;
+    var message = document.getElementById("messageInput").value;
+    var selectedDate = document.getElementById("ttlInput").value;
+    var maleRadio = document.getElementById("maleRadio");
+    var femaleRadio = document.getElementById("femaleRadio");
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var formattedTime = hours + ":" + minutes + ":" + seconds;
+    var selectedGender = "";
+
+    if (name === "" || message === "" || selectedDate === "" || (!maleRadio.checked && !femaleRadio.checked)) {
+        alert("Mohon isi semua input teks!");
+        return;
+    }
+
+    if (maleRadio.checked) {
+        selectedGender = maleRadio.value;
+    } else if (femaleRadio.checked) {
+        selectedGender = femaleRadio.value;
+    }
+
+    document.getElementById("displayValues").innerHTML = "Waktu Submit : " + formattedTime + "<br>" + "<br>" + "Nama : " + name + "<br>" +
+        "Tanggal lahir : " + selectedDate + "<br>" +
+        "Jenis Kelamin : " + selectedGender + "<br>" + "Pesan : " + message;
 }
